410cefa3928574374ebf0bf569d3783a7fa60dfb
swanini <swannewaltz@gmail.com>
2025-04-24 17:08:55 +0900

    headChange2

--- app/View/layouts/head.php
+++ app/View/layouts/head.php
@@ -12,8 +12,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport"
         content="width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no">
-    <meta name="description" content="수화니니 니니앱 스와니니 swanini NiNiApp">
-    <meta name="keywords" content="수화니니 니니앱 스와니니 swanini NiNiApp">
+    <meta name="description" content="수화니니 니니앱 swanini NiNiApp">
+    <meta name="keywords" content="수화니니 니니앱 swanini NiNiApp">
     <meta property="og:url" content="https://php.swanini.com/">
     <meta property="og:title" content="수화니니 | 수화니니 공식 사이트">
     <meta property="og:type" content="website">
