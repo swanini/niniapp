20ce12504e38cd720478afa8ff6ad3f89c1d985d
swanini <swannewaltz@gmail.com>
2025-04-24 16:52:50 +0900

    NiNiHead

--- app/View/layouts/head.php
+++ app/View/layouts/head.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="UTF-8">
-    <title>DB 연결 확인</title>
+    <title>NiNiApp</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="nini/css/startup.css">
 </head>
